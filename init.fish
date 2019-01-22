0ef7f9b4cb65e2a6c77ac1d536015809a173c659
Gabriel Dunn <gabe.dunn@shaw.ca>
2019-01-22 13:36:11 -0700

    add unused init & install scripts

--- init.fish
+++
@@ -1,6 +0,0 @@
-# yadd initialization hook
-#
-# You can use the following variables in this file:
-# * $package       package name
-# * $path          package path
-# * $dependencies  package dependencies

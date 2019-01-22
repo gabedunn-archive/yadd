337f5948d8bb3f83a74fd7aae503d796e9873657
Gabriel Dunn <gabe.dunn@shaw.ca>
2019-01-22 13:09:13 -0700

    add yadd

---
+++ init.fish
@@ -0,0 +1,6 @@
+# yadd initialization hook
+#
+# You can use the following variables in this file:
+# * $package       package name
+# * $path          package path
+# * $dependencies  package dependencies
